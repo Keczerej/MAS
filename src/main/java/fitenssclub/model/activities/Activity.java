335287a56daa68d4f9ddef05a339590d23a53644
Keczerej <pr.jereczek@gmail.com>
2019-06-15 11:21:13 +0200

    jdoc

--- src/main/java/fitenssclub/model/activities/Activity.java
+++ src/main/java/fitenssclub/model/activities/Activity.java
@@ -1,16 +1,17 @@
 package fitenssclub.model.activities;
 
 import fitenssclub.database.ActivityEntity;
-import fitenssclub.database.ExerciseEntity;
-import fitenssclub.model.users.User;
 import fitenssclub.model.users.client.Client;
 import fitenssclub.model.users.worker.roles.ITrainer;
 
-import java.io.*;
+import java.io.Serializable;
 import java.time.LocalDateTime;
 import java.util.*;
 import java.util.stream.Collectors;
 
+/**
+ * Klasa przechowująca informację o zajęciach
+ */
 public class Activity implements Serializable {
 
     private String name;
@@ -22,6 +23,12 @@ public class Activity implements Serializable {
 
     List<ActivityToExercise> exercises = new ArrayList<>(); //Asocjacja z atrybutem
 
+    /**
+     * @param name Nazwa zajęć
+     * @param date Czas rozpoczęcia
+     * @param trainer Przypisany trener
+     * @param room Sala, w której się odbywają
+     */
     public Activity(String name, LocalDateTime date, ITrainer trainer, Room room) {
         this.name = name;
         this.date = date;
@@ -32,14 +39,23 @@ public class Activity implements Serializable {
         ActivityEntity.getInstance().add(this);
     }
 
+    /**
+     * @return nazwa zajęć
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * @param #name nowa nazwa dla zajęć
+     */
     public void setName(String name) {
         this.name = name;
     }
 
+    /**
+     * @return czas rozpoczęcia
+     */
     public LocalDateTime getDate() {
         return date;
     }
@@ -48,10 +64,17 @@ public class Activity implements Serializable {
         this.date = date;
     }
 
+    /**
+     * @return lista uczestników
+     */
     public List<Client> getContributors() {
         return new ArrayList<>(contributors.values());
     }
 
+    /**
+     * Dodaje nowego uczestnika (jeżeli już istnieje - to go nadpisuje)
+     * @param contributor uczestnik do dodania
+     */
     public void addContributor(Client contributor) {
         Optional<Client> existing = this.getContributor(contributor.getLogin());
         existing.ifPresent(client -> this.removeContributor(client.getLogin()));
@@ -59,10 +82,21 @@ public class Activity implements Serializable {
         contributor.addActivity(this);
     }
 
+    /**
+     * Dodanie ćwiczenia razem z czasem
+     *
+     * @param exercise ćwiczenie
+     * @param minutes czas trwamoa w minutach
+     */
     public void addExercise(Exercise exercise, long minutes){
         new ActivityToExercise(exercise, this, minutes);
     }
 
+    /**
+     * Usuwa podane ćwiczenie z listy ćwiczeń. Ignoruje usunięcie gdy nie istanieje.
+     *
+     * @param exercise ćwieczenie do usunięcia
+     */
     public void removeExercise(Exercise exercise){
         this.exercises.forEach(activityToExercise -> {
             if(activityToExercise.getExercise() == exercise) {
@@ -71,8 +105,14 @@ public class Activity implements Serializable {
         });
     }
 
+    /**
+     * Pobierz uczestnika po loginie
+     *
+     * @param login login uczestnika
+     * @return uczestnika lub null gdy nie istnieje
+     */
     //asocjacja z klasyfikatorem
-    public Optional<Client> getContributor(String login){
+    private Optional<Client> getContributor(String login){
         return Optional.ofNullable(this.contributors.get(login));
     }
     public void removeContributor(String login){
@@ -82,18 +122,30 @@ public class Activity implements Serializable {
         }
     }
 
+    /**
+     * @return przypisany trener
+     */
     public ITrainer getTrainer() {
         return trainer;
     }
 
+    /**
+     * @param trainer nowy trener
+     */
     public void setTrainer(ITrainer trainer) {
         this.trainer = trainer;
     }
 
+    /**
+     * @return lista ćwiczeń
+     */
     public List<Exercise> getExercises() {
         return exercises.stream().map(ActivityToExercise::getExercise).collect(Collectors.toList());
     }
 
+    /**
+     * @return zwraca sumę czasu ćwiczeń
+     */
     public long getExercisesTime() {
         return this.exercises.stream().mapToLong(ActivityToExercise::getMinutesForExercise).sum();
     }
@@ -109,10 +161,16 @@ public class Activity implements Serializable {
                 '}';
     }
 
+    /**
+     * @return sala
+     */
     public Room getRoom() {
         return room;
     }
 
+    /**
+     * @param #room nowa sala
+     */
     public void setRoom(Room room) {
         this.room = room;
     }
--- src/main/java/fitenssclub/model/activities/ActivityToExercise.java
+++ src/main/java/fitenssclub/model/activities/ActivityToExercise.java
@@ -3,13 +3,21 @@ package fitenssclub.model.activities;
 import java.io.Serializable;
 
 
+/**
+ *  Przechowuje relację wiążącą zajęcoa z ćwiczeniem
+ */
 public class ActivityToExercise implements Serializable {
 
     private Exercise exercise;
     private Activity activity;
     private long minutesForExercise;
 
-    public ActivityToExercise(Exercise exercise, Activity activity, long minutesForExercise) {
+    /**
+     * @param exercise ćwiczenie
+     * @param activity zajęcia
+     * @param minutesForExercise czas trwania ćwiczenia w trakcie zajęć
+     */
+    ActivityToExercise(Exercise exercise, Activity activity, long minutesForExercise) {
         this.exercise = exercise;
         this.activity = activity;
         this.minutesForExercise = minutesForExercise;
@@ -17,23 +25,38 @@ public class ActivityToExercise implements Serializable {
         this.activity.exercises.add(this);
     }
 
+    /**
+     * @return ćwieczenie
+     */
     public Exercise getExercise() {
         return exercise;
     }
 
+    /**
+     * @return zajęcia
+     */
     public Activity getActivity() {
         return activity;
     }
 
-    public long getMinutesForExercise() {
+    /**
+     * @return czas ćwiczenia
+     */
+    long getMinutesForExercise() {
         return minutesForExercise;
     }
 
+    /**
+     * @param minutesForExercise nowy czas ćwiczeń
+     */
     public void setMinutesForExercise(long minutesForExercise) {
         this.minutesForExercise = minutesForExercise;
     }
 
-    public void removeAssocation(){
+    /**
+     * Usuwa tą relację z obiektów, których dotyczy
+     */
+    void removeAssocation(){
         this.activity.exercises.remove(this);
         this.exercise.activities.remove(this);
     }
