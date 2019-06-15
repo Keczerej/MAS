32ce74675e0ef9d95aa03e941ed48da6c9192ba0
Keczerej <pr.jereczek@gmail.com>
2019-06-15 09:52:39 +0200

    fin

--- src/main/java/fitenssclub/view/ActivityList.java
+++ src/main/java/fitenssclub/view/ActivityList.java
@@ -4,29 +4,20 @@ import fitenssclub.database.Database;
 import fitenssclub.model.activities.Activity;
 
 import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
 public class ActivityList extends AbstractView {
     JPanel activityPanel;
-    private JList listOfActivities;
+    private JList<Activity> listOfActivities;
     private JButton dodajNoweZajęciaButton;
     private JButton anulujButton;
 
     ActivityList(MainController mainController) {
         super(mainController);
-        dodajNoweZajęciaButton.addActionListener(e -> {
-            mainController.openAddNewActivity();
-        });
-        anulujButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent actionEvent) {
-                mainController.openMainMenu();
-            }
-        });
+        dodajNoweZajęciaButton.addActionListener(e -> mainController.openAddNewActivity());
+        anulujButton.addActionListener(actionEvent -> mainController.openMainMenu());
     }
 
-    public void loadActivities() {
+    void loadActivities() {
         DefaultListModel<Activity> model = new DefaultListModel<>();
         Database.getActivities().forEach(activity -> {
             System.out.println(activity);
